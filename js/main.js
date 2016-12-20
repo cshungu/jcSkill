fd13205fc50b3ee3a5fee13514f183e354405d59
silex <christ.skc@gmail.com>
2016-12-20 16:23:05 +0100

    A jour

--- js/main.js
+++ js/main.js
@@ -4,10 +4,9 @@
     var options = {
         color: "#000",
         figure:"square", // By default circle [ circle, square, progress, auto],
-        number: 5
+        number: 10
     };
     JsSkills.set('langage',[{ name:'html/css', level: 5},{ name:'javascript', level:4},{ name:'c#', level:2},{ name:'php', level:4}])
             .set('framework',[{ name:'symfony',level:3},{ name:'jquery', level:4},{ name:'angular', level:3}])
-            .op(options)
-            .init();
+            .op(options).init();
 })();
